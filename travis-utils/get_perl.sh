5a692e63e90dc394c849f1227806cce07ca498d9
Kent Fredric <kentnl@gentoo.org>
2017-11-21 19:39:14 +1300

    Switch to .xz, grumble grumble gumble

--- travis-utils/get_perl.sh
+++ travis-utils/get_perl.sh
@@ -1,2 +1,2 @@
 einfo "Fetching ${TARGET_PERL}"
-wget -O /tmp/perl.tar.bz2 ${TARGET_PERL} || die "Cannot fetch ${TARGET_PERL}";
+wget -O /tmp/perl.tar.xz ${TARGET_PERL} || die "Cannot fetch ${TARGET_PERL}";
