53bca43875b0d66603ec4bc7b7d4ff30024fc8bf
Kent Fredric <kentnl@gentoo.org>
2020-06-25 21:54:50 +1200

    Update patch cleaning code

--- travis-utils/clean-patches.sh
+++ travis-utils/clean-patches.sh
@@ -1,6 +1,6 @@
 set -e -E -o pipefail
 patchdir=$(pwd)/patches
-for suffix in 12-create-libperl-soname.diff; do
+for suffix in 0012-Set-libperl*; do
   einfo "Marking $suffix for exclusion";
   rm -vf "${patchdir}/${suffix}" || die "Excluding ${suffix} failed";
 done
