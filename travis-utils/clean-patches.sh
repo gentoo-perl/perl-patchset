3efe9076d9eccac45cf4b42633b85fdeefe60dc4
Kent Fredric <kentnl@gentoo.org>
2020-06-25 23:13:48 +1200

    update patch strip

--- travis-utils/clean-patches.sh
+++ travis-utils/clean-patches.sh
@@ -1,6 +1,11 @@
 set -e -E -o pipefail
 patchdir=$(pwd)/patches
-for suffix in 0012-Set-libperl*; do
+
+STRIP=( $(
+  cd "${patchdir}";
+  echo 0012*
+  ) )
+for suffix in "${STRIP[@]}"; do
   einfo "Marking $suffix for exclusion";
   rm -vf "${patchdir}/${suffix}" || die "Excluding ${suffix} failed";
 done
